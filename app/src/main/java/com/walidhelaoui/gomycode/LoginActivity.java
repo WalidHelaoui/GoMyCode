5f7951d9bc7fd255b8481a257d4b989daf756c49
Walid Helaoui <he.walid@gmail.com>
2017-12-23 21:42:48 +0100

    fix login issues with firebase

--- app/src/main/java/com/walidhelaoui/gomycode/LoginActivity.java
+++ app/src/main/java/com/walidhelaoui/gomycode/LoginActivity.java
@@ -1,13 +1,71 @@
 package com.walidhelaoui.gomycode;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+    EditText login_edt,password_edt;
+    Button loginBtn;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_edt = findViewById(R.id.edtUsername);
+        password_edt = findViewById(R.id.edtPassword);
+        loginBtn = findViewById(R.id.btn_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    startSignIn();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void startSignIn(){
+         String login = login_edt.getText().toString().trim();
+         String password = password_edt.getText().toString().trim();
+
+         mAuth.signInWithEmailAndPassword(login,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+             @Override
+             public void onComplete(@NonNull Task<AuthResult> task) {
+                 if(task.isSuccessful()){
+                     Toast.makeText(LoginActivity.this,"sign in",Toast.LENGTH_SHORT).show();
+                     Log.e(TAG,"aaa");
+                 }else {
+                     Log.e(TAG,"bbb");
+
+                 }
+             }
+         });
     }
 }
