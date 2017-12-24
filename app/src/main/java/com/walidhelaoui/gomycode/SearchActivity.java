e650e30b9074a905cf7ae413397b4c6267698b29
Walid Helaoui <he.walid@gmail.com>
2017-12-24 02:43:40 +0100

    fix some issues in search activity

--- app/src/main/java/com/walidhelaoui/gomycode/SearchActivity.java
+++ app/src/main/java/com/walidhelaoui/gomycode/SearchActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,8 +25,10 @@ public class SearchActivity extends AppCompatActivity {
 
     private ListView listView;
     private Handler mHandler;
+    CheckBox all,food,hotels,events;
     EditText editText;
     Button search;
+    List<Data> searchList = new ArrayList<>();
     private final Runnable m_Runnable = new Runnable()
     {
         public void run()
@@ -50,30 +53,77 @@ public class SearchActivity extends AppCompatActivity {
         this.mHandler = new Handler();
         this.mHandler.postDelayed(m_Runnable,3000);
 
+        all = findViewById(R.id.AllcheckBox);
+        food = findViewById(R.id.FoodcheckBox);
+        hotels = findViewById(R.id.HotelscheckBox);
+        events = findViewById(R.id.EventscheckBox);
+
         editText = findViewById(R.id.editText);
         search = findViewById(R.id.button2);
         listView = (ListView) findViewById(R.id.SearchListView);
         DataSource.setData(this);
         DataAdapter adapter = new DataAdapter(this, DataSource.datas);
         listView.setAdapter(adapter);
+
     }
 
     public void searchAction(View view) {
        Toast.makeText(this,editText.getText().toString(),Toast.LENGTH_SHORT).show();
-       Float budget = Float.valueOf(editText.getText().toString().trim());
+       Float budget = 0f;
+       try {
+           budget = Float.valueOf(editText.getText().toString().trim());
+       }catch (Exception ex){
+          Toast.makeText(SearchActivity.this,"Enter a Budget",Toast.LENGTH_SHORT).show();
+       }
        List<Data> data = DataSource.datas;
-       List<Data> search = new ArrayList<>();
 
-        for (Data item : data){
-           if ((item.getPrice())<=budget){
-               search.add(item);
-               Log.e("loop","aa");
+       if(all.isChecked()){
+           searchList.clear();
+           for (Data item : data){
+               if ((item.getPrice())<=budget){
+                   searchList.add(item);
+                   Log.e("loop","aa");
+               }
+           }
+       }
+       if(food.isChecked()){
+           for (Data item : data){
+               if (((item.getPrice())<=budget)&&(item.getType().equals("Food"))){
+                   searchList.add(item);
+                   Log.e("loop","aa");
+               }
+           }
+       }
+       if(hotels.isChecked()){
+           for (Data item : data){
+               if (((item.getPrice())<=budget)&&(item.getType().equals("Hotels"))){
+                   searchList.add(item);
+                   Log.e("loop","aa");
+               }
+           }
+       }
+       if(events.isChecked()){
+           for (Data item : data){
+               if (((item.getPrice())<=budget)&&(item.getType().equals("Event"))){
+                   searchList.add(item);
+                   Log.e("loop","aa");
+               }
            }
        }
 
-       DataAdapter adapter = new DataAdapter(SearchActivity.this,search);
+       if((!all.isChecked())&&(!food.isChecked())&&(!hotels.isChecked())&&(!events.isChecked())){
+           searchList.clear();
+       }
+
+       DataAdapter adapter = new DataAdapter(SearchActivity.this,searchList);
        listView.setAdapter(adapter);
 
 
+    }
+    public void clearAction(View view) {
+        searchList.clear();
+        DataAdapter adapter = new DataAdapter(SearchActivity.this,searchList);
+        listView.setAdapter(adapter);
+        editText.setText("");
     }
 }
